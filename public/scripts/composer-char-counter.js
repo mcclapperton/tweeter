3d580a9d2895b1e264dc1c8930c488facf2585e0
Maris <mcclapperton@gmail.com>
2022-11-09 04:06:48 -0700

    added trickey charcter counter as well as counter truning red when chars >140

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+// doc loaded and ready
+$(document).ready(function () {
+  console.log("you ready to write a tweeter??");
+  $(`#tweet-text`).on("input", onInput);
+});
+// counts the characters as theyre input (from 140)
+const onInput = function () {
+  const charRemains = 140 - $(this).val().length;
+  const $counter = $(this).siblings("div").children(".counter");
+  $counter.html(charRemains);
+
+  // counter turns red after 140
+  charRemains < 0
+    ? $counter.addClass("invalid")
+    : $counter.removeClass("invalid");
+};
